95e0b8ee8601d1797cf380ed6e9c2c8e01961dd8
rochaleonardo1986 <rocha_leonardo@hotmail.com>
2018-01-03 13:35:42 -0200

    Update bitcoingui.cpp

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -979,7 +979,7 @@ void BitcoinGUI::dropEvent(QDropEvent *event)
         if (nValidUrisFound)
             gotoSendCoinsPage();
         else
-            notificator->notify(Notificator::Warning, tr("URI handling"), tr("URI can not be parsed! This can be caused by an invalid CultoftheDeadCow address or malformed URI parameters."));
+            notificator->notify(Notificator::Warning, tr("URI handling"), tr("URI can not be parsed! This can be caused by an invalid bitmillion address or malformed URI parameters."));
     }
 
     event->acceptProposedAction();
@@ -994,7 +994,7 @@ void BitcoinGUI::handleURI(QString strURI)
         gotoSendCoinsPage();
     }
     else
-        notificator->notify(Notificator::Warning, tr("URI handling"), tr("URI can not be parsed! This can be caused by an invalid CultoftheDeadCow address or malformed URI parameters."));
+        notificator->notify(Notificator::Warning, tr("URI handling"), tr("URI can not be parsed! This can be caused by an invalid bitmillion address or malformed URI parameters."));
 }
 
 void BitcoinGUI::setEncryptionStatus(int status)
